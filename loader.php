64ba0d5e9e229126cc60a8cfb4f9e0c9d18c63dc
shanebp <shanebp@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-03-14 13:57:15 +0000

    tagging 1.5
    git-svn-id: https://plugins.svn.wordpress.org/bp-local-avatars/tags/1.5@1112584 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- loader.php
+++ loader.php
@@ -2,7 +2,7 @@
 /*
 Plugin Name: BP Local Avatars
 Description: Requires BuddyPress. Adds an option to create an Identicon Avatar and store it locally.
-Version: 1.2
+Version: 1.5
 Author: PhiloPress
 Author URI: http://philopress.com/
 */
